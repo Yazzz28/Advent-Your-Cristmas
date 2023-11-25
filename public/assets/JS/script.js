d890d3ba0d601e24d4c71a1e647402b081f3ea06
Yazzz28 <yacinemennaa@gmail.com>
2023-11-25 09:28:13 +0100

    fix api non secure

--- public/assets/JS/script.js
+++ public/assets/JS/script.js
@@ -12,49 +12,24 @@ function ouverture(i) {
     document.getElementById(i).querySelector(".front").classList.toggle("open");
 }
 
-
 function generateContent() {
-    
-    //Génére une blague aléatoire
+    // Génère une blague aléatoire
     fetch('https://api.blablagues.net/?rub=blagues')
-    .then(response => response.json())
-    .then(data => {
-        const blague = document.getElementById("blague");
-        blague.innerHTML = `${data.data.content.text}${data.data.content.text_head}\n${data.data.content.text_hidden}`
-    })
-
-    //Génére une idée de filme
-    const options = {
-        method: 'GET',
-        headers: {
-          accept: 'application/json'
-      };
-
-
-      const randomPage = Math.floor(Math.random() * (2 - 0 ) + 1);
-      console.log(randomPage);
-
-      const url = "https://api.themoviedb.org/3/search/collection?query=christmas&include_adult=false&language=fr-FR&page=1";
-      console.log(url);
-
-      fetch(url, options)
         .then(response => response.json())
-        .then(response => {
-            const randomNumber = Math.floor(Math.random() * 17)
-            const nameFilmRandom = response.results[randomNumber].original_name
-            document.getElementById('film').setAttribute("value", nameFilmRandom);
-
+        .then(data => {
+            const blague = document.getElementById("blague");
+            blague.innerHTML = `${data.data.content.text}${data.data.content.text_head}\n${data.data.content.text_hidden}`;
         })
-            
-        .catch(err => console.error(err));
+        .catch(err => console.log(err));
 
+
+    // Génère une idée de cadeau et une recette
     fetch("/selectCadeauRandom")
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('cadeau').setAttribute("value", data.cadeau.idea);
-        console.log(data)
-        document.getElementById('title_recipe').setAttribute("value", data.recette.title);
-    })
-
-
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('cadeau').setAttribute("value", data.cadeau.idea);
+            console.log(data)
+            document.getElementById('title_recipe').setAttribute("value", data.recette.title);
+        })
+        .catch(err => console.error(err));
 }
